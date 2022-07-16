187e77e95cbc52f72835dffc747417b9cdb69552
Tạ Phước Ánh <taphuocanh@huaf.edu.vn>
2022-07-16 16:46:09 +0700

    create procedure module and component

---
+++ src/app/main/procedure/procedure.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProcedureComponent } from './procedure.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProcedureComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProcedureModule { }
